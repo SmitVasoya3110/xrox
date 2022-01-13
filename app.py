bc8349520a751e2e8c6e58a3a4ec8bb60cfb9841
SmitVasoya3110 <68841925+SmitVasoya3110@users.noreply.github.com>
2022-01-13 14:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,17 +44,36 @@ app.config['MYSQL_DB'] = "print"
 mysql = MySQL(app)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 24
 
-MIME = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword', 'application/vnd.oasis.opendocument.text-master']
+MIME = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword',
+        'application/vnd.oasis.opendocument.text-master']
+ABN = 16612402767
+COMPANY = "Printing 7 Bondi"
+
 
 @app.errorhandler(413)
 def too_large(e):
-    return {"message":"File/s is/are too large. limit is 24 MB","limit":"24 MB"}, 413
+    return {"message": "File/s is/are too large. limit is 24 MB", "limit": "24 MB"}, 413
+
 
 @app.errorhandler(500)
 def internal_error(e):
-    return {"error" : "There is Internal Server Error"}
+    return {"error": "There is Internal Server Error"}
+
 
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'doc', 'docx'}
+
+
+def check_email(email):
+    qry = "select Email_Id from Customer_Master where Email_Id = %s"
+    # cur = mysql2.connection.cursor()
+    cur = mysql.connection.cursor()
+    cur.execute(qry, (email,))
+    result = cur.fetchone()
+    cur.close()
+    if result:
+        return 1
+    else:
+        return 0
 
 
 def A4_BC(num: int):
@@ -145,7 +164,7 @@ def CustomerLogin():
             return jsonify({"message": "Enter Valid Email_Id or Password"}), 401
 
         # mysql.connection.close()
-        if (len(rows) > 0):
+        if len(rows) > 0:
             dic1 = {}
 
             dic2 = {}
@@ -176,17 +195,6 @@ def CustomerLogin():
         print(e)
         return ({"error": "There was an error"})
 
-def check_email(email):
-    qry = "select Email_Id from Customer_Master where Email_Id = %s"
-    # cur = mysql2.connection.cursor()
-    cur = mysql.connection.cursor()
-    cur.execute(qry, (email,))
-    result = cur.fetchone()
-    cur.close()
-    if result:
-        return 1
-    else:
-        return 0
 
 @app.route("/Customer", methods=["POST"])
 def register_user():  # add new Customer -- MYSQL table : Customer_Master
@@ -230,46 +238,46 @@ def register_user():  # add new Customer -- MYSQL table : Customer_Master
         return {}, 400
 
 
-@app.route('/register', methods=['POST'])
-def register():
-    @copy_current_request_context
-    def send_email(receiver):
-        msg = Message('Welcome to Print', sender=app.config['MAIL_USERNAME'], recipients=[receiver])
-        print(msg)
-        msg.body = welcome_message
-        mail.send(msg)
-
-    start = time.perf_counter()
-    if request.method == 'POST':
-        content_type = request.headers.get('Content-Type')
-        if content_type == 'application/json':
-            json_data = request.json
-            email = json_data.get('email', 0)
-            first_name = json_data.get('first_name', 0)
-            last_name = json_data.get('last_name', 0)
-            password = json_data.get('password', 0)
-            mobile = int(json_data.get('mobile', 0))
-            print(email, first_name, last_name, password, mobile)
-            if email and first_name and last_name and password and mobile:
-                qry = "insert into user (email, password, first_name, last_name, mobile) values (%s,%s,%s,%s,%s)"
-                values = (email, password, first_name, last_name, mobile)
-                cur = mysql.connection.cursor()
-                cur.execute(qry, values)
-                mysql.connection.commit()
-                # t1 = Process(target=send_email, args=(email,))
-                # t1.start()
-                # # t1.join()
-                # send_email(email)
-                thread = threading.Thread(target=send_email, args=(email,))
-                thread.start()
-                return {"Success": "Inserted Successfully"}
-            else:
-                print("Seconds", time.perf_counter() - start)
-                return "Values are missing or not correct"
-        else:
-            return 'Content-Type not supported!'
-
-    return "Server is Up and running"
+# @app.route('/register', methods=['POST'])
+# def register():
+#     @copy_current_request_context
+#     def send_email(receiver):
+#         msg = Message('Welcome to Print', sender=app.config['MAIL_USERNAME'], recipients=[receiver])
+#         print(msg)
+#         msg.body = welcome_message
+#         mail.send(msg)
+#
+#     start = time.perf_counter()
+#     if request.method == 'POST':
+#         content_type = request.headers.get('Content-Type')
+#         if content_type == 'application/json':
+#             json_data = request.json
+#             email = json_data.get('email', 0)
+#             first_name = json_data.get('first_name', 0)
+#             last_name = json_data.get('last_name', 0)
+#             password = json_data.get('password', 0)
+#             mobile = int(json_data.get('mobile', 0))
+#             print(email, first_name, last_name, password, mobile)
+#             if email and first_name and last_name and password and mobile:
+#                 qry = "insert into user (email, password, first_name, last_name, mobile) values (%s,%s,%s,%s,%s)"
+#                 values = (email, password, first_name, last_name, mobile)
+#                 cur = mysql.connection.cursor()
+#                 cur.execute(qry, values)
+#                 mysql.connection.commit()
+#                 # t1 = Process(target=send_email, args=(email,))
+#                 # t1.start()
+#                 # # t1.join()
+#                 # send_email(email)
+#                 thread = threading.Thread(target=send_email, args=(email,))
+#                 thread.start()
+#                 return {"Success": "Inserted Successfully"}
+#             else:
+#                 print("Seconds", time.perf_counter() - start)
+#                 return "Values are missing or not correct"
+#         else:
+#             return 'Content-Type not supported!'
+#
+#     return "Server is Up and running"
 
 @app.route('/multiple-files-upload', methods=['POST'])
 @jwt_required()
@@ -332,7 +340,7 @@ def upload_file():
             errors[file.filename] = 'File type is not allowed'
     num_dict['Total_Pages'] = total_pages
     if size == "A4" and typ.lower() == 'color':
-        num_dict['Total_cost'] = round(A4_C(total_pages),2)
+        num_dict['Total_cost'] = round(A4_C(total_pages), 2)
     if size == "A4" and typ.lower() == 'bw':
         num_dict['Total_cost'] = round(A4_BC(total_pages), 2)
     if size == "A3" and typ.lower() == 'color':
@@ -357,6 +365,7 @@ def upload_file():
 
 job_msg = "Your job as an email posted"
 
+
 @app.route('/place/order', methods=["POST"])
 @jwt_required()
 def place_order():
@@ -379,6 +388,7 @@ def place_order():
     except Exception as e:
         return {"message": "Internal Server Error"}
 
+
 #
 # @app.route("/get", methods=["GET"])
 # def fun():
@@ -394,15 +404,12 @@ def place_order():
 #     print(res)
 #     return {"Res": res}
 
-send = """
-Your Order have been placed. following are the details
-"""
 
 @jwt_required()
 @app.route('/confirm/order', methods=["POST"])
 def confirm_payment():
     @copy_current_request_context
-    def send_attachment(order_id: int, files: list, receiver: str):
+    def send_attachment(order_id: int, files: list, amount:int,receiver: str):
         msg = Message('Order', sender=app.config['MAIL_USERNAME'], recipients=[app.config['ORDER_MAIL']])
         msg.body = f"Order has been received with <order_id:{order_id}> from <{receiver}>"
         print(files)
@@ -410,14 +417,14 @@ def confirm_payment():
             file = secure_filename(file)
             print(file)
             nme = os.path.join(app.config['UPLOAD_FOLDER'], file)
-            print("Full Path.....=>",(os.path.join(app.config['UPLOAD_FOLDER'], file)))
+            print("Full Path.....=>", (os.path.join(app.config['UPLOAD_FOLDER'], file)))
             buf = open(nme, 'rb').read()
             print(magic.from_buffer(buf, mime=True))
             msg.attach(file, magic.from_buffer(buf, mime=True), buf)
         print(msg)
         mail.send(msg)
-        msg = Message(job_msg, sender=app.config['MAIL_USERNAME'], recipients=[receiver])
-        msg.body = send + f"\norder_id:{order_id} \n files:{files}"
+        msg = Message("Customer Receipt", sender=app.config['MAIL_USERNAME'], recipients=[receiver])
+        msg.body = f"\norder_id:{order_id} \n files:{files} \n Price:{amount} \n ABN:{ABN} \n Company:{COMPANY}"
         mail.send(msg)
 
     json_data = request.json
@@ -433,90 +440,91 @@ def confirm_payment():
         cur = mysql.connection.cursor()
         cur.execute(qry, (order_id, user_id, amount, 1))
         mysql.connection.commit()
-        threading.Thread(target=send_attachment, args=(order_id, files, email)).start()
+        threading.Thread(target=send_attachment, args=(order_id, files, amount,email)).start()
         return {"message": "OK"}, 200
 
 
-@jwt_required()
-@app.route('/uploads', methods=["POST"])
-def attach_mail():
-    files_details = []
-
-    @copy_current_request_context
-    def send_with_attachment(receiver, files):
-        msg = Message(job_msg, sender=app.config['MAIL_USERNAME'], recipients=[receiver])
-        print(msg)
-        msg.body = "Your files"
-        for file in files:
-            msg.attach(file[0].filename, file[1],
-                       open(os.path.join(app.config['UPLOAD_FOLDER'], file[0].filename), 'rb').read())
-        mail.send(msg)
-
-    for files in request.files.getlist('files[]'):
-        files_details.append([files, files.mimetype])
-    print(files_details)
-    threading.Thread(target=send_with_attachment, args=('ssssmmmmiiiitttt@gmail.com', files_details)).start()
-    return jsonify({"message": "OK"})
+# @jwt_required()
+# @app.route('/uploads', methods=["POST"])
+# def attach_mail():
+#     files_details = []
+#
+#     @copy_current_request_context
+#     def send_with_attachment(receiver, files):
+#         msg = Message(job_msg, sender=app.config['MAIL_USERNAME'], recipients=[receiver])
+#         print(msg)
+#         msg.body = "Your files"
+#         for file in files:
+#             msg.attach(file[0].filename, file[1],
+#                        open(os.path.join(app.config['UPLOAD_FOLDER'], file[0].filename), 'rb').read())
+#         mail.send(msg)
+#
+#     for files in request.files.getlist('files[]'):
+#         files_details.append([files, files.mimetype])
+#     print(files_details)
+#     threading.Thread(target=send_with_attachment, args=('ssssmmmmiiiitttt@gmail.com', files_details)).start()
+#     return jsonify({"message": "OK"})
 
 
 @app.route('/forgot-password', methods=['POST'])
 def forgot_password():
     @copy_current_request_context
-    def send_email(receiver,url):
+    def send_email(receiver, url):
         msg = Message(sender=app.config['MAIL_USERNAME'], recipients=[receiver])
         print(msg)
-        msg.body = "Following is the reset password link. It will be expired in 2 hours\n"+str(url)
+        msg.body = "Following is the reset password link. It will be expired in 2 hours\n" + str(url)
         mail.send(msg)
 
     # print("..",_doc_(request))
 
     # url = request.host_url + 'Reset'
-    url="http://localhost:3000/Reset/"
+    url = "http://localhost:3000/Reset/"
     body = request.get_json()
     Email_Id = body.get('Email_Id')
     if not Email_Id:
-        return {"message":"Email_Id is requred"},400
+        return {"message": "Email_Id is requred"}, 400
 
     sql = """
-     SELECT * FROM `Customer_Master` where Email_Id = '"""+str(Email_Id)+"""' and status='1'
+     SELECT * FROM `Customer_Master` where Email_Id = '""" + str(Email_Id) + """' and status='1'
      """
     cur = mysql.connection.cursor()
     cur.execute(sql)
     result = cur.fetchone()
     cur.close()
     if not result:
-        return {"message":"Invaild Email ID"},400
-    print("result",result)
-    print("headers",request.headers)
+        return {"message": "Invalid Email ID"}, 400
+    print("result", result)
+    print("headers", request.headers)
 
     expires = datetime.timedelta(hours=1)
     reset_token = create_access_token(str(result[3]), expires_delta=expires)
     # return 0
-    url+=str(reset_token)
-    print("url",url)
-    thread = threading.Thread(target=send_email, args=(Email_Id,url)).start()
+    url += str(reset_token)
+    print("url", url)
+    thread = threading.Thread(target=send_email, args=(Email_Id, url)).start()
     # thread.start()
     return {"message": "email was send for reset password"}
+
 
 @app.route('/reset-password', methods=['POST'])
 def reset_password():
     body = request.json
-    print("body",body)
-    if body == None:
-        return {"message":"Inavid JSON"},400
+    print("body", body)
+    if body is None:
+        return {"message": "Invalid JSON"}, 400
     reset_token = body.get('reset_token')
     Password = body.get('Password')
     if not reset_token or not Password:
-        return {"message":"Plz Provide reset_token and password"},400
+        return {"message": "Plz Provide reset_token and password"}, 400
 
     user_id = decode_token(reset_token)['sub']
     print(user_id)
-    print("user_id",user_id)
+    print("user_id", user_id)
     if not user_id:
-        return {"message":"Invaid Reset Tokan"},400
+        return {"message": "Invalid Reset Token"}, 400
 
     if not check_email(user_id):
-        return {"message":"Invalid User"}
+        return {"message": "Invalid User"}
     # sql = """
     #  SELECT * FROM `Customer_Master` where id = '"""+str(user_id)+"""' and status='1'
     #  """
@@ -527,18 +535,17 @@ def reset_password():
     # if not result:
     #     return {"message":"Invaild User"},400
 
-
-    sql="""
-    update Customer_Master set Password='"""+str(Password)+"""'
-    where Email_Id='"""+str(user_id)+"""' and status='1'
+    sql = """
+    update Customer_Master set Password='""" + str(Password) + """'
+    where Email_Id='""" + str(user_id) + """' and status='1'
     """
     cur = mysql.connection.cursor()
     cur.execute(sql)
     mysql.connection.commit()
     # result = cursor.fetchone()
     cur.close()
-    return {"message":"Password was reset"}
+    return {"message": "Password was reset"}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
